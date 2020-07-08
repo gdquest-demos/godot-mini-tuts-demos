47a2de783c78e27ea3a61274521bc20718cd73ed
Henrique Campos <henriiquecampos@gmail.com>
2020-07-08 11:36:57 -0300

    Add pickup sound effect demo
    A project to showcase how to prevent sfx chopping using pick up objects as example

---
+++ Audio/Pickup Sound Effects/Coin/Coin.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+onready var animation_player := $AnimationPlayer
+onready var sprite := $Sprite
+
+func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_released("left_click"):
+		animation_player.play("Picked")
+
+
+func _on_Area2D_mouse_entered() -> void:
+	sprite.modulate = Color(1.2, 1.2, 1.2)
+
+
+func _on_Area2D_mouse_exited() -> void:
+	sprite.modulate = Color.white
