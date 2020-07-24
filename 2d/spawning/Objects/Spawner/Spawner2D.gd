8357af42ca078f51ab6c8e036f0ddd5eb537858b
Henrique Campos <henriiquecampos@gmail.com>
2020-07-24 16:18:36 -0300

    Update Spawner2D.gd

--- 2d/spawning/Objects/Spawner/Spawner2D.gd
+++ 2d/spawning/Objects/Spawner/Spawner2D.gd
@@ -3,8 +3,8 @@ extends Position2D
 
 export var spawn_scene: PackedScene
 
-func spawn() -> void:
-	var spawn := spawn_scene.instance() as Node2D
+func spawn(_spawn_scene := spawn_scene) -> void:
+	var spawn := _spawn_scene.instance() as Node2D
 	add_child(spawn)
 	spawn.set_as_toplevel(true)
 	spawn.global_position = global_position
