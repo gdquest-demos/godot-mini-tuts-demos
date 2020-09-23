a3eb9ea5cc010f7f0e69518b353da88e972ab1ec
Nathan Lovato <nathan@gdquest.com>
2020-09-23 08:58:34 -0600

    Update limited vision demo
    Rename Assets folder
    Simplify player code

--- 2d/limited-player-vision/Player.gd
+++ 2d/limited-player-vision/Player.gd
@@ -2,16 +2,14 @@ extends KinematicBody2D
 
 export var speed := 300.0
 
-var direction := Vector2.ZERO
-
-var _velocity := Vector2.ZERO
 
 func _physics_process(delta) -> void:
-  update_direction()
-  _velocity = direction.normalized() * speed
-  _velocity = move_and_slide(_velocity)
+	var direction := calculate_move_direction()
+	move_and_slide(direction * speed)
 
 
-func update_direction() -> void:
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+func calculate_move_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
