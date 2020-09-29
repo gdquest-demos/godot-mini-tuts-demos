f35cca23ee5808cbac45c98cbfba8c7e49bbe659
Francois Belair <razoric480@gmail.com>
2020-09-29 15:25:22 -0400

    Fix index wrap-around bug

--- 2d/character-customization/UserInterface/UISpriteSelector.gd
+++ 2d/character-customization/UserInterface/UISpriteSelector.gd
@@ -15,19 +15,17 @@ func setup(sprite_textures: Array) -> void:
 
 
 func _on_PreviousButton_pressed() -> void:
-	var new_index := (_index - 1) % _sprites.size()
+	var new_index := wrapi(_index - 1, 0, _sprites.size())
 	_set_index(new_index)
 
 
 func _on_NextButton_pressed() -> void:
-	var new_index := (_index + 1) % _sprites.size()
+	var new_index := wrapi(_index + 1, 0, _sprites.size())
 	_set_index(new_index)
 
 
 func _set_index(value: int) -> void:
-	_index = value
-	if value < 0:
-		return
+	_index = clamp(value, 0, _sprites.size()-1)
 	var texture: StreamTexture = _sprites[_index]
 	texture_rect.texture = texture
 	emit_signal("sprite_changed", texture)
