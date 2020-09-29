260529734817c59398da44bfdae456b71df3c889
Nathan Lovato <nathan@gdquest.com>
2020-09-29 11:40:34 -0600

    Refactor character customization demo

--- 2d/character-customization/Player.gd
+++ 2d/character-customization/Player.gd
@@ -2,29 +2,31 @@ extends KinematicBody2D
 
 export var speed := 300.0
 
-var direction := Vector2.ZERO
-
-var _velocity := Vector2.ZERO
-
-func _physics_process(delta) -> void:
-	update_direction()
-	_velocity = direction.normalized() * speed
-	_velocity = move_and_slide(_velocity)
+onready var body := $Body
+onready var hat := $Body/Hat
+onready var glasses := $Body/Glasses
+onready var head := $Body/Head
 
 
-func update_direction() -> void:
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+func _physics_process(_delta) -> void:
+	var direction := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+
+	move_and_slide(direction * speed)
+
 	if direction.x != 0:
-		$Body.scale.x = sign(direction.x)
-	
-	
-func update_hat(hat : Texture) -> void:
-	$Body/Hat.texture = hat
-	
-func update_glasses(glasses : Texture) -> void:
-	$Body/Glasses.texture = glasses
+		body.scale.x = sign(direction.x)
 
-func update_head(head : Texture) -> void:
-	$Body/Head.texture = head
+
+func _on_CharacterCustomizer_glasses_changed(texture) -> void:
+	glasses.texture = texture
+
+
+func _on_CharacterCustomizer_hat_changed(texture) -> void:
+	hat.texture = texture
+
+
+func _on_CharacterCustomizer_head_changed(texture) -> void:
+	head.texture = texture
