f79f73557aa2a952a3322f66ff5f16b83c7f6ea1
Rafael Alejandro Rodríguez Gómez <raformatico@raformatico.com>
2020-10-09 00:37:33 +0200

    First use of gdformat

--- 2d/camera-zoom/UI/Zoom_slider.gd
+++ 2d/camera-zoom/UI/Zoom_slider.gd
@@ -1,11 +1,14 @@
 class_name ZoomSlider
 extends Control
 
-onready var zoom_camera: ZoomingCamera2D = get_node("/root/ZoomingCameraDemo/Player/ZoomingCamera2D")
+onready var zoom_camera: ZoomingCamera2D = get_node(
+	"/root/ZoomingCameraDemo/Player/ZoomingCamera2D"
+)
 onready var zoom_vslider: VSlider = $VSlider
 
+
 func _ready() -> void:
-	zoom_camera.connect("zoom_level_changed",self,"_on_zoom_level_changed")
+	zoom_camera.connect("zoom_level_changed", self, "_on_zoom_level_changed")
 	_on_zoom_level_changed(zoom_camera.zoom_level)
 
 
@@ -14,4 +17,6 @@ func _on_zoom_level_changed(new_value: float) -> void:
 
 
 func _on_VSlider_value_changed(new_value: float) -> void:
-	zoom_camera.set_zoom_level(range_lerp(new_value,100, 1, zoom_camera.min_zoom, zoom_camera.max_zoom))
+	zoom_camera.set_zoom_level(
+		range_lerp(new_value, 100, 1, zoom_camera.min_zoom, zoom_camera.max_zoom)
+	)
