27ac61a4850a93896af308ef508b20ab0334d842
raformatico <raformatico@raformatico.com>
2020-10-08 17:54:19 +0200

    Added ZoomSlider to the demo

--- 2d/camera-zoom/Actors/ZoomingCamera2D.gd
+++ 2d/camera-zoom/Actors/ZoomingCamera2D.gd
@@ -1,30 +1,32 @@
 class_name ZoomingCamera2D
 extends Camera2D
 
+signal zoom_level_changed(new_value)
+
 export var zoom_duration := 0.2
 export var zoom_factor := 0.1
 export var min_zoom := 0.5
 export var max_zoom := 2.0
 
-var _zoom_level := 1.0 setget _set_zoom_level
+var zoom_level := 1.0 setget _set_zoom_level
 
 onready var tween: Tween = $Tween
 
-
 func _unhandled_input(event):
 	if event.is_action_pressed("zoom_in"):
-		_set_zoom_level(_zoom_level - zoom_factor)
+		_set_zoom_level(zoom_level - zoom_factor)
 	if event.is_action_pressed("zoom_out"):
-		_set_zoom_level(_zoom_level + zoom_factor)
+		_set_zoom_level(zoom_level + zoom_factor)
 
 
 func _set_zoom_level(value: float) -> void:
-	_zoom_level = clamp(value, min_zoom, max_zoom)
+	zoom_level = clamp(value, min_zoom, max_zoom)
+	emit_signal("zoom_level_changed",zoom_level)
 	tween.interpolate_property(
 		self,
 		"zoom",
 		zoom,
-		Vector2(_zoom_level, _zoom_level),
+		Vector2(zoom_level, zoom_level),
 		zoom_duration,
 		tween.TRANS_SINE,
 		tween.EASE_OUT
---
+++ 2d/camera-zoom/UI/Zoom_slider.gd
@@ -0,0 +1,17 @@
+class_name ZoomSlider
+extends Control
+
+onready var zoom_camera: ZoomingCamera2D = get_node("/root/ZoomingCameraDemo/Player/ZoomingCamera2D")
+onready var zoom_vslider: VSlider = $VSlider
+
+func _ready() -> void:
+	zoom_camera.connect("zoom_level_changed",self,"_on_zoom_level_changed")
+	_on_zoom_level_changed(zoom_camera.zoom_level)
+
+
+func _on_zoom_level_changed(new_value: float) -> void:
+	zoom_vslider.value = range_lerp(new_value, zoom_camera.min_zoom, zoom_camera.max_zoom, 100, 1)
+
+
+func _on_VSlider_value_changed(new_value: float) -> void:
+	zoom_camera._set_zoom_level(range_lerp(new_value,100, 1, zoom_camera.min_zoom, zoom_camera.max_zoom))
