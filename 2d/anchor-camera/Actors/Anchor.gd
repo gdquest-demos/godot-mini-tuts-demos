9a8b3c9dc18b92d9be7ee9aada10935bcddbe2ad
raformatico <raformatico@raformatico.com>
2020-10-13 02:29:00 +0200

    Version using anchor and zoom

--- 2d/anchor-camera/Actors/Anchor.gd
+++ 2d/anchor-camera/Actors/Anchor.gd
@@ -1,7 +1,9 @@
 class_name Anchor
 extends Area2D
 
+export var zoom_level := 1.0
+
 
 func _on_Anchor_body_entered(body: Node) -> void:
 	if body is Player:
-		EVENTS.emit_signal("player_entered", self.global_position)
+		EVENTS.emit_signal("player_entered", self.global_position, self.zoom_level)
--- 2d/anchor-camera/Actors/AnchorCamera2D.gd
+++ 2d/anchor-camera/Actors/AnchorCamera2D.gd
@@ -4,6 +4,7 @@ extends Camera2D
 export var anchor_duration := 0.5
 
 var _anchor_pos := Vector2.ZERO setget _set_anchor_pos
+var _zoom_level := 1.0 setget _set_zoom_level
 
 onready var tween: Tween = $Tween
 
@@ -26,6 +27,20 @@ func _set_anchor_pos(new_pos: Vector2) -> void:
 	tween.start()
 
 
-func _on_player_entered(new_anchor_position: Vector2) -> void:
+func _set_zoom_level(new_zoom: float) -> void:
+	_zoom_level = new_zoom
+	tween.interpolate_property(
+		self,
+		"zoom",
+		zoom,
+		Vector2(_zoom_level, _zoom_level),
+		anchor_duration,
+		tween.TRANS_SINE,
+		tween.EASE_OUT
+	)
+	tween.start()
+
+func _on_player_entered(new_anchor_position: Vector2, new_zoom_level: float) -> void:
 	_set_anchor_pos(new_anchor_position)
+	_set_zoom_level(new_zoom_level)
 
