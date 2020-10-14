9b5b9a2571cab016768cf230bd1a8d161c1cf033
raformatico <raformatico@raformatico.com>
2020-10-14 17:12:10 +0200

    Anchor camera focused on camera and returning to it

--- 2d/anchor-camera/Actors/AnchorCamera2D.gd
+++ 2d/anchor-camera/Actors/AnchorCamera2D.gd
@@ -10,14 +10,15 @@ onready var tween: Tween = $Tween
 
 
 func _ready() -> void:
-	EVENTS.connect("player_entered", self, "_on_player_entered")
+	EVENTS.connect("camera_to_anchor", self, "_on_camera_to_anchor")
+	EVENTS.connect("camera_to_player", self, "_on_camera_to_player")
 
 
 func _set_anchor_pos(new_pos: Vector2) -> void:
 	_anchor_pos = new_pos
 	tween.interpolate_property(
 		self,
-		"position",
+		"global_position",
 		global_position,
 		_anchor_pos,
 		anchor_duration,
@@ -40,7 +41,29 @@ func _set_zoom_level(new_zoom: float) -> void:
 	)
 	tween.start()
 
-func _on_player_entered(new_anchor_position: Vector2, new_zoom_level: float) -> void:
+
+func _set_camera_to_player(prev_anchor_position: Vector2) -> void:
+	set_as_toplevel(false)
+	global_position = prev_anchor_position
+	tween.interpolate_property(
+		self,
+		"position",
+		position,
+		Vector2.ZERO,
+		anchor_duration,
+		tween.TRANS_SINE,
+		tween.EASE_OUT
+	)
+	tween.start()
+
+
+func _on_camera_to_anchor(new_anchor_position: Vector2, new_zoom_level: float) -> void:
+	set_as_toplevel(true)
 	_set_anchor_pos(new_anchor_position)
 	_set_zoom_level(new_zoom_level)
 
+
+func _on_camera_to_player(prev_anchor_position: Vector2) -> void:
+	if not _zoom_level == 1.0:
+		_set_zoom_level(1.0)
+	_set_camera_to_player(prev_anchor_position)
--- 2d/anchor-camera/Singletone/Events.gd
+++ 2d/anchor-camera/Singletone/Events.gd
@@ -1,6 +1,4 @@
 extends Node
 
-signal zoom_level_changed(new_value)
-signal zoom_slider_changed(new_value)
-signal zoom_anchor_changed(new_anchor_pos)
-signal player_entered(new_anchor_pos, new_zoom_level)
+signal camera_to_player(new_anchor_pos)
+signal camera_to_anchor(new_anchor_pos, new_zoom_level)
