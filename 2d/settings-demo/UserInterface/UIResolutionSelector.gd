ac43e6e493c2c4458c64d60a47d8b5f3f20454a1
azagaya <azagaya.games@gmail.com>
2020-10-14 12:54:26 -0300

    Remove _ready function from UIResolutionSelector
    That function was used to set the default resolution, but it was left out of the tutorial.

--- 2d/settings-demo/UserInterface/UIResolutionSelector.gd
+++ 2d/settings-demo/UserInterface/UIResolutionSelector.gd
@@ -5,10 +5,6 @@ signal resolution_changed(new_resolution)
 onready var option_button: OptionButton = $OptionButton
 
 
-func _ready() -> void:
-	_update_selected_item(0)
-
-
 func _update_selected_item(index: int) -> void:
 	var values := option_button.text.split_floats("x")
 	emit_signal("resolution_changed", Vector2(values[0], values[1]))
