c87dec89a144d2927404d97c4cf19861f834935a
azagaya <azagaya.games@gmail.com>
2020-10-14 12:56:46 -0300

    Change _update_selected to take a string as argument
    This is because we are not using the index of the option button

--- 2d/settings-demo/UserInterface/UIResolutionSelector.gd
+++ 2d/settings-demo/UserInterface/UIResolutionSelector.gd
@@ -5,10 +5,10 @@ signal resolution_changed(new_resolution)
 onready var option_button: OptionButton = $OptionButton
 
 
-func _update_selected_item(index: int) -> void:
-	var values := option_button.text.split_floats("x")
+func _update_selected_item(text: String) -> void:
+	var values := text.split_floats("x")
 	emit_signal("resolution_changed", Vector2(values[0], values[1]))
 
 
-func _on_OptionButton_item_selected(index: int) -> void:
-	_update_selected_item(index)
+func _on_OptionButton_item_selected(_index: int) -> void:
+	_update_selected_item(option_button.text)
