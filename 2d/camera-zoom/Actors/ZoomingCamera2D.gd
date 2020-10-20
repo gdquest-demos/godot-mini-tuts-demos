15f936da4f8d951f8025db30eab0506d9b84a216
Nathan Lovato <nathan@gdquest.com>
2020-10-20 13:42:56 -0600

    Refactor zoom slider code
    Decoupled the UI and camera:
    
    - Made camera's zoom_level property public
    - Wired camera and UI through the parent ZoomingCameraDemo node
    - Used VSlider properties to set the min and max value range instead of
    - using lerp
    - Removed `self.`

--- 2d/camera-zoom/Actors/ZoomingCamera2D.gd
+++ 2d/camera-zoom/Actors/ZoomingCamera2D.gd
@@ -8,39 +8,28 @@ export var zoom_factor := 0.1
 export var min_zoom := 0.5
 export var max_zoom := 2.0
 
-var _zoom_level := 1.0 setget _set_zoom_level
+var zoom_level := 1.0 setget set_zoom_level
 
 onready var tween: Tween = $Tween
-onready var zoom_slider: VSlider = get_node(
-	"/root/ZoomingCameraDemo/UI/Zoom_slider"
-)
-
-
-func _ready() -> void:
-	zoom_slider.connect("vslider_changed", self, "_on_Zoom_slider_vslider_changed")
 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("zoom_in"):
-		_set_zoom_level(_zoom_level - zoom_factor)
+		set_zoom_level(zoom_level - zoom_factor)
 	if event.is_action_pressed("zoom_out"):
-		_set_zoom_level(_zoom_level + zoom_factor)
+		set_zoom_level(zoom_level + zoom_factor)
 
 
-func _set_zoom_level(value: float) -> void:
-	_zoom_level = clamp(value, min_zoom, max_zoom)
-	self.emit_signal("zoom_level_changed", _zoom_level)
+func set_zoom_level(value: float) -> void:
+	zoom_level = clamp(value, min_zoom, max_zoom)
+	emit_signal("zoom_level_changed", zoom_level)
 	tween.interpolate_property(
 		self,
 		"zoom",
 		zoom,
-		Vector2(_zoom_level, _zoom_level),
+		Vector2(zoom_level, zoom_level),
 		zoom_duration,
 		tween.TRANS_SINE,
 		tween.EASE_OUT
 	)
 	tween.start()
-
-
-func _on_Zoom_slider_vslider_changed(new_value: float) -> void:
-	_set_zoom_level(range_lerp(new_value, 100, 1, min_zoom, max_zoom))
--- 2d/camera-zoom/UI/Zoom_slider.gd
+++ 2d/camera-zoom/UI/Zoom_slider.gd
@@ -1,21 +1,13 @@
 class_name ZoomSlider
 extends VSlider
 
-signal vslider_changed(new_value)
 
-onready var zoom_camera: ZoomingCamera2D = get_node(
-	"/root/ZoomingCameraDemo/Player/ZoomingCamera2D"
-)
-
-
-func _ready() -> void:
-	zoom_camera.connect("zoom_level_changed", self, "_on_ZoomingCamera2D_zoom_level_changed")
-	_on_ZoomingCamera2D_zoom_level_changed(zoom_camera._zoom_level)
+func setup(camera: ZoomingCamera2D) -> void:
+	camera.connect("zoom_level_changed", self, "update_value")
+	min_value = camera.min_zoom
+	max_value = camera.max_zoom
+	value = camera.zoom_level
 
 
-func _on_ZoomingCamera2D_zoom_level_changed(new_value: float) -> void:
-	self.value = range_lerp(new_value, zoom_camera.min_zoom, zoom_camera.max_zoom, 100, 1)
-
-
-func _on_VSlider_value_changed(new_value: float) -> void:
-	self.emit_signal("vslider_changed", new_value)
+func update_value(new_value: float) -> void:
+	value = new_value
