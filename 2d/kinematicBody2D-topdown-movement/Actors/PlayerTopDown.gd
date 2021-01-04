445c03f98356fb7607b80ebb4ecc248ed2a247b0
Nathan Lovato <nathan@gdquest.com>
2021-01-04 08:56:41 -0600

    Edited PlayerTopDown.gd
    Minor tweaks:
    
    - Renamed some variables.
    - Normalized the direction when calculating and storing it.

--- 2d/kinematicBody2D-topdown-movement/Actors/PlayerTopDown.gd
+++ 2d/kinematicBody2D-topdown-movement/Actors/PlayerTopDown.gd
@@ -1,32 +1,34 @@
 extends KinematicBody2D
 
+# Movement speed in pixels per second.
 export var speed := 500
 
-var _frame_index := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
+# Mapping of direction to a sprite index.
+var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
 var _velocity := Vector2.ZERO
 
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
 
-func _physics_process(delta):
+func _physics_process(_delta: float) -> void:
 	var direction := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
-	var target_velocity = direction.normalized() * speed
-	move_and_slide(target_velocity)
+	).normalized()
+	move_and_slide(speed * direction)
 
 
+# The code below updates the character's sprite to look in a specific direction.
 func _unhandled_input(event):
 	if event.is_action_pressed("right"):
-		look_to(Vector2.RIGHT)
+		_update_sprite(Vector2.RIGHT)
 	elif event.is_action_pressed("left"):
-		look_to(Vector2.LEFT)
+		_update_sprite(Vector2.LEFT)
 	elif event.is_action_pressed("down"):
-		look_to(Vector2.DOWN)
+		_update_sprite(Vector2.DOWN)
 	elif event.is_action_pressed("up"):
-		look_to(Vector2.UP)
+		_update_sprite(Vector2.UP)
 
 
-func look_to(direction: Vector2) -> void:
-	animated_sprite.frame = _frame_index[direction]
+func _update_sprite(direction: Vector2) -> void:
+	animated_sprite.frame = _sprites[direction]
