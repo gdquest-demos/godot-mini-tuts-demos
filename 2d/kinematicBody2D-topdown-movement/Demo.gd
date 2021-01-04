31b94963f98498cc85209ab0441e00844908cb0e
Nathan Lovato <nathan@gdquest.com>
2021-01-04 09:11:23 -0600

    Address loading and input errors

--- 2d/kinematicBody2D-topdown-movement/Demo.gd
+++ 2d/kinematicBody2D-topdown-movement/Demo.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 var topdown_player := load("res://Actors/PlayerTopDown.tscn")
 var topdown_acc_player := load("res://Actors/PlayerTopDownAcceleration.tscn")
-var grid_player := load("res://Actors/PlayerGrid.tscn")
-var grid_tween_player := load("res://Actors/PlayerGridTween.tscn")
 var rotate_move_player := load("res://Actors/PlayerRotate.tscn")
 var platformer_player := load("res://Actors/PlayerPlatformer.tscn")
 
@@ -13,10 +11,6 @@ func _unhandled_input(event):
 		substitute_player(topdown_player.instance())
 	elif event.is_action_pressed("load_topdown_acc"):
 		substitute_player(topdown_acc_player.instance())
-	elif event.is_action_pressed("load_grid"):
-		substitute_player(grid_player.instance())
-	elif event.is_action_pressed("load_grid_tween"):
-		substitute_player(grid_tween_player.instance())
 	elif event.is_action_pressed("load_rotate_move"):
 		substitute_player(rotate_move_player.instance())
 	elif event.is_action_pressed("load_platformer"):
