54f793ebf4474456b4d43008db30048ddee5ebe4
Nathan Lovato <nathan@gdquest.com>
2021-01-04 12:44:06 -0600

    Quick code cleanup
    Renamed src/ -> Sprites/ as the folder contains sprites
    Cleaned up style and names in some GDScript code

--- 2d/SideScrolling-Movements/Actors/PlayerAngry.gd
+++ 2d/SideScrolling-Movements/Actors/PlayerAngry.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 export var impulse := 1000
 
-var angry_bullet: PackedScene = load("res://Actors/PlayerAngryBullet.tscn")
+const AngryBullet: PackedScene = preload("res://Actors/PlayerAngryBullet.tscn")
 
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("shoot"):
-		var angry_node: RigidBody2D = angry_bullet.instance()
-		angry_node.global_position = global_position
-		angry_node.linear_velocity = global_transform.x * impulse
-		owner.add_child(angry_node)
+		var bullet: RigidBody2D = AngryBullet.instance()
+		bullet.global_position = global_position
+		bullet.linear_velocity = global_transform.x * impulse
+		owner.add_child(bullet)
--- 2d/SideScrolling-Movements/Actors/PlayerGeometry.gd
+++ 2d/SideScrolling-Movements/Actors/PlayerGeometry.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
 export var gravity := 4000
-export var jump_speed := -1400
-export var minimum_speed := 400
+export var jump_impulse := 1400
+export var speed := 400
 
 var _velocity := Vector2.ZERO
 
 
 func _physics_process(delta) -> void:
-	_velocity.x = minimum_speed
+	_velocity.x = speed
 	_velocity.y += gravity * delta
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-			_velocity.y = jump_speed
+			_velocity.y = -jump_impulse
