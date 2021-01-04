7937eb47ca66a95917a0d8b4658d3c22558f7863
raformatico <raformatico@raformatico.com>
2021-01-04 23:45:08 +0100

    Player Platformer deleted and minor issue about changing players fixed
    - New scene per player (easier and safer for changing them)
    - Relocate Demo.tscn and Demo.gd to use them as root for inherited scenes

--- 2d/kinematicBody2D-topdown-movement/Levels/SceneIndicator.gd
+++ 2d/kinematicBody2D-topdown-movement/Levels/SceneIndicator.gd
@@ -2,10 +2,7 @@ extends MarginContainer
 
 
 func _ready() -> void:
-	var children = get_node("/root/Demo/Level1").get_children()
-	for child in children:
-		if "Player" in child.name:
-			$ColorRect/Label.text = child.name
+	$ColorRect/Label.text = get_node("/root/").get_child(0).name
 
 
 func update_label(node_name: String) -> void:
