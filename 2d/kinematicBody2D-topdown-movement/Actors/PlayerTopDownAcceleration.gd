1f0b34435021fdb51c078c97daa09f55f161053b
Nathan Lovato <nathan@gdquest.com>
2021-01-04 09:11:35 -0600

    Edit PlayerTopDownAcceleration.gd, use steering instead of lerp
    I prefer to teach vector math than using lerp; the calculations are
    always simpler and control's easier than with lerp factors.

--- 2d/kinematicBody2D-topdown-movement/Actors/PlayerTopDownAcceleration.gd
+++ 2d/kinematicBody2D-topdown-movement/Actors/PlayerTopDownAcceleration.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
+# Movement speed in pixels per second.
 export var speed := 500
-export var friction = 0.1
-export var acceleration = 0.05
+# A factor that controls the character's inertia.
+export var friction = 0.18
 
-var _frame_index := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
+# Mapping of direction to a sprite index.
+var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
 var _velocity := Vector2.ZERO
 
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
@@ -14,25 +16,24 @@ func _physics_process(delta):
 	var direction := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
-	var target_velocity = direction.normalized() * speed
-	if target_velocity.is_equal_approx(Vector2.ZERO):
-		_velocity = lerp(_velocity, Vector2.ZERO, friction)
-	else:
-		_velocity = lerp(_velocity, target_velocity, acceleration)
+	).normalized()
+	# Using the follow steering behavior.
+	var target_velocity = direction * speed
+	_velocity += (target_velocity - _velocity) * friction
 	_velocity = move_and_slide(_velocity)
 
 
+# The code below updates the character's sprite to look in a specific direction.
 func _unhandled_input(event):
 	if event.is_action_pressed("right"):
-		look_to(Vector2.RIGHT)
+		_update_sprite(Vector2.RIGHT)
 	elif event.is_action_pressed("left"):
-		look_to(Vector2.LEFT)
+		_update_sprite(Vector2.LEFT)
 	elif event.is_action_pressed("down"):
-		look_to(Vector2.DOWN)
+		_update_sprite(Vector2.DOWN)
 	elif event.is_action_pressed("up"):
-		look_to(Vector2.UP)
+		_update_sprite(Vector2.UP)
 
 
-func look_to(direction: Vector2) -> void:
-	animated_sprite.frame = _frame_index[direction]
+func _update_sprite(direction: Vector2) -> void:
+	animated_sprite.frame = _sprites[direction]
