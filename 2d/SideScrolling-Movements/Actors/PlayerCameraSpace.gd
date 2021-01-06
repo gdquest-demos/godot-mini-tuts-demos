a9b54010583e9da92145670980d7ec90e4340a11
Nathan Lovato <nathan@gdquest.com>
2021-01-05 20:18:51 -0600

    Fix jitter in PlayerCameraSpace.gd

--- 2d/SideScrolling-Movements/Actors/PlayerCameraSpace.gd
+++ 2d/SideScrolling-Movements/Actors/PlayerCameraSpace.gd
@@ -3,5 +3,5 @@ extends Node2D
 export var minimum_speed := Vector2(400, 0)
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	position += minimum_speed * delta
