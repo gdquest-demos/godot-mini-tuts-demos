71377f3ff037999c3a98d8ca3320b4f53905e522
Nathan Lovato <nathan@gdquest.com>
2021-01-13 07:11:57 -0600

    Minor code style tweaks

--- shaders/silhouette2D/scenes/Guard.gd
+++ shaders/silhouette2D/scenes/Guard.gd
@@ -4,6 +4,7 @@ export var speed := 100.0
 
 onready var animated_sprite := $Viewport/AnimatedSprite
 
+
 func _physics_process(_delta) -> void:
 	var direction := Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
