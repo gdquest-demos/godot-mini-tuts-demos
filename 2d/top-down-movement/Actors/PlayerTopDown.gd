2a17ca383e603029efb6ea94f0ab3a08e6c6d985
raformatico <raformatico@raformatico.com>
2021-03-07 00:39:08 +0100

    Adapt code and inputs to work with analogue controllers

--- 2d/top-down-movement/Actors/PlayerTopDown.gd
+++ 2d/top-down-movement/Actors/PlayerTopDown.gd
@@ -14,7 +14,13 @@ func _physics_process(_delta: float) -> void:
 	var direction := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized()
+	)
+	# When aiming the joystick diagonally, the direction vector can have a length 
+	# greater than 1.0, making the character move faster than our maximum expected
+	# speed. When that happens, we limit the vector's length to ensure the player 
+	# can't go beyond the maximum speed.
+	if direction.length() > 1.0:
+		direction = direction.normalized()
 	move_and_slide(speed * direction)
 
 
--- 2d/top-down-movement/Actors/PlayerTopDownAcceleration.gd
+++ 2d/top-down-movement/Actors/PlayerTopDownAcceleration.gd
@@ -16,7 +16,13 @@ func _physics_process(delta):
 	var direction := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized()
+	)
+	# When aiming the joystick diagonally, the direction vector can have a length 
+	# greater than 1.0, making the character move faster than our maximum expected
+	# speed. When that happens, we limit the vector's length to ensure the player 
+	# can't go beyond the maximum speed.
+	if direction.length() > 1.0:
+		direction = direction.normalized()
 	# Using the follow steering behavior.
 	var target_velocity = direction * speed
 	_velocity += (target_velocity - _velocity) * friction
