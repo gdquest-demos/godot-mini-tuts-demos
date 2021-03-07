bf9fbcee8e72d6b684efb892c85e8b6ed4183bc1
raformatico <raformatico@raformatico.com>
2021-03-07 12:20:25 +0100

    Rotating with a different Joy axis

--- 2d/top-down-movement/Actors/PlayerRotate.gd
+++ 2d/top-down-movement/Actors/PlayerRotate.gd
@@ -8,7 +8,7 @@ export var angular_speed := 5.0
 
 
 func _physics_process(delta):
-	var rotate_direction := Input.get_action_strength("right") - Input.get_action_strength("left")
+	var rotate_direction := Input.get_action_strength("rotate_right") - Input.get_action_strength("rotate_left")
 	rotation += rotate_direction * angular_speed * delta
 	# `transform.y` stores the node's local axes, allowing us to move it in the direction it's currently facing.
 	var velocity := (Input.get_action_strength("down") - Input.get_action_strength("up")) * transform.y * speed
